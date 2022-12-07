460390af638aac57ad853fca871ec65b4408cc6f
Chayan Chandro Sutrodhar <109588297+ChayanSD@users.noreply.github.com>
2022-12-07 22:44:50 +0600

    Add files via upload

---
+++ Recursion/Nto1.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class Nto1 {
+    public static void main(String[] args) {
+        int n =5;
+        print(n);
+    }
+    static void print(int n){
+        if(n==0){
+            return;
+        }
+        System.out.print(n +" ");
+        print(n-1);
+    }
+
+}
