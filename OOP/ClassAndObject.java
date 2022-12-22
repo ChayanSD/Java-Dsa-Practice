9d9bd7d1eb36c8d9bec2472aa2609098d6c5cf8a
Chayan Chandro Sutrodhar <109588297+ChayanSD@users.noreply.github.com>
2022-12-22 22:24:37 +0600

    Create ClassAndObject.java

---
+++ OOP/ClassAndObject.java
@@ -0,0 +1,13 @@
+package OOP;
+
+public class ClassandObject {
+    public static void main(String[] args) {
+        Student chayan = new Student();//this is the object.
+    }
+}
+class Student{
+    //this is the class.
+    int roll;
+    String age;
+    float marks;
+}
