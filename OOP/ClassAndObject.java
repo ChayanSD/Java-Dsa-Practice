3a355d4af8fcc130f35261e475179f0e0226994e
Chayan Chandro Sutrodhar <109588297+ChayanSD@users.noreply.github.com>
2023-01-03 22:30:25 +0600

    Update ClassAndObject.java

--- OOP/ClassAndObject.java
+++ OOP/ClassAndObject.java
@@ -1,13 +1,20 @@
 package OOP;
-
-public class ClassandObject {
+public class ClassAndObject {
     public static void main(String[] args) {
-        Student chayan = new Student();//this is the object.
+        Student student1 = new Student();//this is the object.
+        student1.roll=12;
+        student1.name="Chayan SD";
+        student1.marks=88.4f;
+        System.out.println(student1.name);
+        System.out.println(student1.roll);
+        System.out.println(student1.marks);
+
     }
 }
 class Student{
     //this is the class.
+    //for every student
     int roll;
-    String age;
+    String name;
     float marks;
 }
